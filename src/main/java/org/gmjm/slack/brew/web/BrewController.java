22bca2f94f3bb48dd8398444caead1a0e4683815
Andy Glassman <aglassman@digitalmeasures.com>
2016-03-29 15:53:14 -0500

    Allows accepted tokens to be configurable.

--- src/main/java/org/gmjm/slack/brew/web/BrewController.java
+++ src/main/java/org/gmjm/slack/brew/web/BrewController.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 final class BrewController
 {
 
-	private static final List<String> tokens =
-		Arrays.asList("cXwoODqDUNni1M4B9uj9xJsR","UllRXghdloI4JOnaeE77y27s");
+	@Value("#{'${slack.brew.valid_command_tokens}'.split(',')}")
+	private List<String> tokens;
 
 	private static final Logger logger = LoggerFactory.getLogger(BrewController.class);
 
